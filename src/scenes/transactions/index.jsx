7010a409202516bf02d5fcccb323f9f77995228d
Kevin Torres <kevin.torres@kushkipagos.com>
2022-12-22 15:44:32 -0500

    add get transactions request

---
+++ src/scenes/transactions/index.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import { DataGrid } from "@mui/x-data-grid";
+import { useGetTransactionsQuery } from "state/api";
+import Header from "components/Header";
+import { useTheme } from "@mui/material";
+
+const Transactions = () => {
+  const theme = useTheme();
+  const [page, setPage] = useState(0);
+  const [pageSize, setPageSize] = useState(20);
+  const [sort, setSort] = useState({});
+  const [search, setSearch] = useState("");
+
+  const { data, isLoading } = useGetTransactionsQuery({
+    page,
+    pageSize,
+    sort: JSON.stringify(sort),
+    search,
+  });
+
+  return <div>Transactions</div>;
+};
+
+export default Transactions;
