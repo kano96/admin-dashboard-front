2e426da159aaebeedd058bf4d0bb39109c7a857c
Kevin Torres <kevin.torres@kushkipagos.com>
2022-12-27 17:38:07 -0500

    add overview chart

--- src/utils/dateUtils.js
+++ src/utils/dateUtils.js
@@ -2,3 +2,33 @@ import format from "date-fns/format";
 
 export const getFullDateFormatted = (created) =>
   `${format(new Date(created), "dd-MM-yyyy | HH:mm:ss")}`;
+
+export const getMonthByIndex = (index) => {
+  switch (index) {
+    case 0:
+      return "January";
+    case 1:
+      return "February";
+    case 2:
+      return "March";
+    case 3:
+      return "April";
+    case 4:
+      return "May";
+    case 5:
+      return "June";
+    case 6:
+      return "July";
+    case 7:
+      return "August";
+    case 8:
+      return "September";
+    case 9:
+      return "October";
+    case 10:
+      return "November ";
+    case 11:
+    default:
+      return "December";
+  }
+};
