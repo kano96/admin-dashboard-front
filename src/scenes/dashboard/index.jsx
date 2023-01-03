05e0083fd3a26199d738822add14ca725288f3f2
Kevin Torres <kevin.torres@kushkipagos.com>
2023-01-03 14:27:12 -0500

    add buttons and header at dashboard

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -1,5 +1,86 @@
+import FlexBetween from "components/FlexBetween";
+import Header from "components/Header";
+import {
+  DownloadOutlined,
+  Email,
+  PointOfSale,
+  PersonAdd,
+  Traff,
+} from "@mui/icons-material";
+import {
+  Box,
+  Button,
+  Typography,
+  useTheme,
+  useMediaQuery,
+} from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
+import BreakdownChart from "components/BreakdownChart";
+import OverviewChart from "components/OverviewChart";
+import { useGetDashboardQuery } from "state/api";
+import { getFullDateFormatted } from "utils/dateUtils";
+
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const theme = useTheme();
+  const isNonMediumScreens = useMediaQuery("(min-width: 1200px)");
+  const { data, isLoading } = useGetDashboardQuery();
+
+  const columns = [
+    {
+      field: "_id",
+      headerName: "ID",
+      flex: 1,
+    },
+    {
+      field: "userId",
+      headerName: "User ID",
+      flex: 1,
+    },
+    {
+      field: "createdAt",
+      headerName: "Created At",
+      flex: 1,
+      renderCell: (date) => getFullDateFormatted(date.value),
+    },
+    {
+      field: "products",
+      headerName: "# of Products",
+      flex: 0.5,
+      sortable: false,
+      renderCell: (params) => params.value.length,
+    },
+    {
+      field: "cost",
+      headerName: "Cost",
+      flex: 1,
+      renderCell: (params) => `$${Number(params.value).toFixed(2)}`,
+    },
+  ];
+
+  return (
+    <Box m="1.5rem 2.5rem">
+      <FlexBetween>
+        <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />
+        <Box>
+          <Button
+            sx={{
+              backgroundColor: theme.palette.secondary.light,
+              color: theme.palette.background.alt,
+              fontSize: "14px",
+              fontWeight: "bold",
+              padding: "10px 20px",
+              "&:hover": {
+                background: theme.palette.secondary.light,
+              },
+            }}
+          >
+            <DownloadOutlined sx={{ mr: "10px" }} />
+            Download Reports
+          </Button>
+        </Box>
+      </FlexBetween>
+    </Box>
+  );
 };
 
 export default Dashboard;
--- src/state/api.js
+++ src/state/api.js
@@ -12,6 +12,7 @@ export const api = createApi({
     "Sales",
     "Admins",
     "Performance",
+    "Dashboard",
   ],
   endpoints: (builder) => ({
     getUser: builder.query({
@@ -50,6 +51,10 @@ export const api = createApi({
       query: (id) => `management/performance/${id}`,
       providesTags: ["Performance"],
     }),
+    getDashboard: builder.query({
+      query: () => "general/dashboard",
+      providesTags: ["Dashboard"],
+    }),
   }),
 });
 
@@ -62,4 +67,5 @@ export const {
   useGetSalesQuery,
   useGetAdminsQuery,
   useGetUserPerformanceQuery,
+  useGetDashboardQuery,
 } = api;
