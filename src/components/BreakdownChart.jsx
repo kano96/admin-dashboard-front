44cbc9ed367d03339e2a9c1c20e18da91fb94ec5
Kevin Torres <kevin.torres@kushkipagos.com>
2023-01-04 11:17:29 -0500

    fix chart to await for data

--- src/components/BreakdownChart.jsx
+++ src/components/BreakdownChart.jsx
@@ -11,7 +11,7 @@ const BreakdownChart = ({ isDashboard = false }) => {
     theme.palette.secondary[300],
     theme.palette.secondary[500],
   ];
-  const formattedData = Object.entries(data.salesByCategory).map(
+  const formattedData = Object.entries(data?.salesByCategory).map(
     ([category, sales], i) => ({
       id: category,
       label: category,
